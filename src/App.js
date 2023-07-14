5b3bcdb99369df14b305d7dcd9bc58725fe57853
Moises Pahati <moisespahati@gmail.com>
2023-07-14 15:18:44 +0800

    add conditional rendering and logic to step UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 
 export default function App() {
+  const step = 3;
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
       </div>
-      <p className="message">Hello</p>
-      <div className="button">
-        <button>Previous</button>
-        <button>Next</button>
+      <p className="message">Step {step}: { messages[step - 1]}</p>
+      <div className="buttons">
+        <button style={{backgroundColor: "#7950f2", color: "#fff"}}>Previous</button>
+        <button style={{backgroundColor: "#7950f2", color: "#fff"}}>Next</button>
       </div>
     </div>
   );  
