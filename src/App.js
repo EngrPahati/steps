6d3739505881090b3c9ebc17eed13f030b9128b0
Moises Pahati <moisespahati@gmail.com>
2023-07-15 08:23:45 +0800

    refactor numbers className - remove template literals

--- src/App.js
+++ src/App.js
@@ -22,12 +22,13 @@ export default function App() {
     }
   }
 
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+        <div className={step >= 1 ? 'active' : ''}>1</div>
+        <div className={step >= 2 ? 'active' : ''}>2</div>
+        <div className={step >= 3 ? 'active' : ''}>3</div>
       </div>
       <p className="message">Step {step}: { messages[step - 1]}</p>
       <div className="buttons">
