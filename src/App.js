9f750e70bda58f43a3767bff3fd3453fece590c3
Moises Pahati <moisespahati@gmail.com>
2023-07-15 16:44:35 +0800

    use callback function to update state

--- src/App.js
+++ src/App.js
@@ -13,20 +13,20 @@ export default function App() {
 
   function handleNext() {
     if (step < 3) {
-      setStep(step + 1);
+      setStep(s => s + 1);
     }
   }
 
   function handlePrevious() {
     if (step > 1) {
-      setStep(step - 1);
+      setStep(s => s - 1);
     }
   }
 
 
   return (
     <>
-      <button className='close' onClick={() => setIsOpen(!isOpen)}>&times;</button>
+      <button className='close' onClick={() => setIsOpen(is => !is)}>&times;</button>
       {isOpen && (
         
         < div className="steps" >
